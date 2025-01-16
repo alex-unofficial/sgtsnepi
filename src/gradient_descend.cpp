359cf8a508a8c44a55e89a0604d02899c9627f82
alex-unofficial <athanasiadisalex2@gmail.com>
2025-01-16 23:41:39 +0200

    another debug statement

--- src/gradient_descend.cpp
+++ src/gradient_descend.cpp
@@ -307,6 +307,7 @@ void kl_minimization(coord* y,
             << "%]" << std::endl;
 
   printf("%s:%d: after print\n", __FILE__, __LINE__);
+  printf("%s:%d: before delete\n", __FILE__, __LINE__);
 
   delete [] dy;
   delete [] uy;
